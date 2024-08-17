63187a7f9a049a7973d226fe8ae132f65d5f2e64
Guillermo Santos <guillermo.santosba@gmail.com>
2024-08-17 17:28:06 -0600

    Receive event from socket.

--- UVGTour/Data/Datasource/SocketIOSensorDatasource.swift
+++ UVGTour/Data/Datasource/SocketIOSensorDatasource.swift
@@ -7,17 +7,86 @@
 
 import Foundation
 import Combine
+import SocketIO
+
 
 /// Datasource that integrates SocketIO for sensor simulation.
-struct SocketIOSensorDatasource: SensorRepository {
+class SocketIOSensorDatasource: SensorRepository {
+    private var webSocketTask: URLSessionWebSocketTask?
+    private var sensorsSubject = PassthroughSubject<[Sensor], Never>()
+    
+    init () {
+        listenToSocket()
+    }
+    
+    deinit {
+        webSocketTask = nil
+    }
+    
     var sensorsPublisher: AnyPublisher<[Sensor], Never> {
-        Timer.publish(every: 1.0, on: .main, in: .common)
-            .autoconnect()
-            .map { _ in
-                [Sensor(id: "123", distance: Float.random(in: 0...5)), Sensor(id: "1234", distance: Float.random(in: 0...5))]
-            }
+        sensorsSubject
+            .receive(on: RunLoop.main)  // Ensure values are received on the main thread
             .eraseToAnyPublisher()
     }
+
+    
+    
+    // MARK: Socket
+    
+    /// Listents to socket updates, configures the events to update the UI.
+    func listenToSocket() {
+        let url = URL(string: "ws://127.0.0.1:8000")!
+        let urlSession = URLSession(configuration: .default)
+        webSocketTask = urlSession.webSocketTask(with: url)
+        webSocketTask?.resume()
+        
+        receiveMessage()  // Start receiving messages
+    }
+    
+    /// Configures the receive of a message
+    func receiveMessage() {
+        webSocketTask?.receive { [weak self] result in
+            switch result {
+            case .failure(let error):
+                print("Error receiving message: \(error)")
+            case .success(let message):
+                switch message {
+                case .string(let text):
+                    self?.handleMessage(text)
+                case .data(let data):
+                    print("Received data: \(data)")
+                @unknown default:
+                    fatalError()
+                }
+                
+                // Continue to listen for the next message
+                self?.receiveMessage()
+            }
+        }
+    }
+    
+    /// Handles a message
+    func handleMessage(_ text: String) {
+            // Handle your JSON message here
+            if let data = text.data(using: .utf8) {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    if let dictionary = json as? [String: Any], let type = dictionary["type"] as? String, type == "sensors-update" {
+                        // Handle the sensor update
+                        let sensorsRawData = dictionary["data"] as? Array<[String:Any]>
+                        print(sensorsRawData?[0] ?? "")
+                        let webSocketSensors = sensorsRawData?.compactMap({ sensorData in
+                            try? WebSocketSensor.fromJson(sensorData)
+                        }) ?? []
+                        sensorsSubject.send(webSocketSensors.map({ webSocketSensor in
+                            Sensor(id: webSocketSensor.id, distance: webSocketSensor.signalStrength)
+                        }))
+                    }
+                } catch {
+                    print("Error parsing JSON: \(error)")
+                }
+            }
+        }
     
     
 }
--- UVGTour/UVGTourApp.swift
+++ UVGTour/UVGTourApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct UVGTourApp: App {
+
     var body: some Scene {
+        let datasource = SocketIOSensorDatasource()
+        let repository = SensorRepositoryImpl(datasource: datasource)
+        let useCase = WatchSensorsUseCase(sensorsRepository: repository)
+        let viewModel = TestSensorViewModel(watchSensorsUseCase: useCase)
         WindowGroup {
-            ContentView()
+            TestSensorScreen(viewModel: viewModel)
         }
     }
 }
