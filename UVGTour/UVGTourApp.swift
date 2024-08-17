b18d6aa586c2c057865279754aae1b9280fc51d4
Guillermo Santos <guillermo.santosba@gmail.com>
2024-08-17 14:27:09 -0600

    Initial commit

---
+++ UVGTour/UVGTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  UVGTourApp.swift
+//  UVGTour
+//
+//  Created by Guillermo Santos Barrios on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct UVGTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
